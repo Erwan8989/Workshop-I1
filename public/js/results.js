2086efb13c6d3ab39a20597055d1705663c470cb
Erwan <erwan8989@outlook.fr>
2023-10-11 13:52:57 +0100

    ...

--- public/js/results.js
+++ public/js/results.js
@@ -5,7 +5,6 @@ let player1 = localStorage.getItem("player1") ? localStorage.getItem("player1") 
 let player2 = localStorage.getItem("player2") ? localStorage.getItem("player2") : "Joueur 2";
 
 let t = localStorage.getItem("questions");
-console.log(JSON.parse(t));
 
 joueur1.innerHTML = player1;
 joueur2.innerHTML = player2;
