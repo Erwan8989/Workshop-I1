e9148fa5a7029ee935fdd69ad01d2e7e82de8ac1
Erwan <erwan8989@outlook.fr>
2023-10-12 09:22:53 +0100

    Update parameters.js

--- public/js/parameters.js
+++ public/js/parameters.js
@@ -4,6 +4,8 @@ document.querySelector("form").addEventListener("submit", function (event) {
     var player1Name = document.getElementById("player1").value;
     var player2Name = document.getElementById("player2").value;
 
+    localStorage.clear();
+
     localStorage.setItem("player1", player1Name);
     localStorage.setItem("player2", player2Name);
 
