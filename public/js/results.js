1e7f4bbb42591e08ec8220077b0a86c3f243c529
Theo77-art <77727097+Theo77-art@users.noreply.github.com>
2023-10-12 12:03:33 +0200

    wip

--- public/js/results.js
+++ public/js/results.js
@@ -41,21 +41,126 @@ if (isPlayer1winner() === true) {
 function getAnsweredQuestions(){
     const questions = JSON.parse(localStorage.getItem("questions"));
     let answeredQuestions = [];
-
-    console.log(questions);
     
     for(let i = 0;  i < questions.length; i++) {
-        console.log(questions[i]["reponse_donnees"]);
         if(questions[i]["reponse_donnees"]){
-            break;
+            answeredQuestions.push(questions[i]);
         } else{
-             answeredQuestions.push(questions[i]);
+            break;
         }
     }
     return answeredQuestions;
 }
 
-console.log("ttt");
+function isIntEven(number){
+
+    number = parseInt(number);
+
+    if(number%2 === 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+function constructQuestionRecap(answeredQuestions){
+
+    const block = document.getElementById("game-recap");
+    var sections = "";
+
+    for(var i = 0; i < answeredQuestions.length; i++){
+        var questionNumber = constructQuestionNumber(i);
+        var questionText = '<div class="question-text">' + answeredQuestions[i]["description"]+'</div>';
+        var questionExplanation = '<div class="question-explanation">'+ answeredQuestions[i]["explication"] +'</div>';
+
+        var questionResponse = constructQuestionButtons(answeredQuestions[i]);
+
+        var questionElement = questionNumber + questionText + questionResponse + questionExplanation;
+
+
+        if(isIntEven(i)){
+           var questionSection = '<div class="question-section">'+ questionElement+'</div>';
+        }else{
+            var questionSection = '<div class="question-section left">'+ questionElement+'</div>';
+        }
+
+        sections += questionSection
+    }
+
+
+    block.innerHTML = sections;
+}
+
+
+function constructQuestionNumber(i){
+    if(isIntEven(i)){
+        var questionNumber = '<div class="question-number blue">Question ' + i + '</div>';
+    }else {
+        var questionNumber = '<div class="question-number red">Question ' + i + '</div>';
+    }
+
+    return questionNumber;
+}
+
+
+function constructQuestionButtons(question){
+    var buttons = "";
+
+    console.log(question["reponses"]);
+
+    for(var i = 0; i < question["reponses"].length; i++){
+        if(question["reponses"][i] === question["bonne_reponse"]){
+            buttons += '<button class="btn btn-success" type="button">'+ question["reponses"][i] +'</button>';
+        }else if(question["reponses"][i] === question["reponse_donnees"]) {
+            buttons += '<button class="btn btn-danger" type="button">'+question["reponses"][i]+'</button>';
+        }else{
+            buttons += '<button class="btn btn-info" type="button">'+question["reponses"][i]+'</button>'
+        }
+    }
+
+    var questionResponses = '<div class="question-response">'+ buttons +'</div>';
+    
+
+    return questionResponses; 
+}
+
+
+function questionObjectReorder(question){
+
+    var responses = [];
+
+    responses.push(question["bonne_reponse"]);
+    responses.push(question["reponse_2"]);
+    delete question["reponse_2"];
+
+    if(question["reponse_3"]){
+        responses.push(question["reponse_3"]);
+        delete question["reponse_3"];
+    }
+    if(question["reponse_4"]){
+        responses.push(question["reponse_4"]);
+        delete question["reponse_4"];
+    }
+
+    question["reponses"] = responses;
+
+    return question;
+}
+
+function reorderQuestionsArray(array){
+    var reorderedQuestions = [];
+
+    for(var i=0; i<array.length; i++){
+        reorderedQuestions[i] = questionObjectReorder(array[i]);
+    }
+
+    console.log(reorderedQuestions);
+
+    return reorderedQuestions;
+}
+
+
 
 
 joueur1.innerHTML = namePlayer1;
@@ -68,7 +173,9 @@ player1Score.innerHTML = isFirstLetterVowel(namePlayer1) ? "Score d'" + namePlay
 player2Score.innerHTML = isFirstLetterVowel(namePlayer2) ? "Score d' " + namePlayer2 : "Score de " + namePlayer2;
 
 
-const answeredQuestions = getAnsweredQuestions()
+var answeredQuestions = getAnsweredQuestions()
 
-console.log("outside");
-console.log(answeredQuestions);
+
+answeredQuestions = reorderQuestionsArray(answeredQuestions);
+
+constructQuestionRecap(answeredQuestions);
